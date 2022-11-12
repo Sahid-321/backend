4d7d7fac82e916a403c8e3f9fe1cdc56ce4d6cb4
sahid jamal <sahidjamal151@gmail.com>
2022-11-12 19:12:19 +0530

    partial added

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
+const { hslToRgb } = require("@material-ui/core");
 const express = require("express");
 const app = express();
-
+const hbs = require("hbs")
 const port = 3000;
 require("./db/conn")
 
 const path = require("path");
 
 const static_page = path.join(__dirname,"./public")
+const templates_page = path.join(__dirname,"./templates/views")
+const partial_page = path.join(__dirname,"./templates/partials")
+
 //console.log(path.join(__dirname,"./public"));
 
 app.use(express.static(static_page));
 
 app.set("view engine", "hbs");
+app.set("views",templates_page);
+
+hbs.registerPartials(partial_page);
 
 
 
