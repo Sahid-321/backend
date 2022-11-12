053c5bd86b7f1b0eaeb25478cd1df26c5c2fc99a
sahid jamal <sahidjamal151@gmail.com>
2022-11-12 18:53:07 +0530

    hbs added

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const app = express();
 
+const port = 3000;
+require("./db/conn")
+
 const path = require("path");
 
 const static_page = path.join(__dirname,"./public")
 //console.log(path.join(__dirname,"./public"));
 
-app.use(express.static(static_page))
-const port = 3000;
-require("./db/conn")
+app.use(express.static(static_page));
+
+app.set("view engine", "hbs");
+
+
+
 app.get("/",(req,res) =>{
-    res.send("hello I am sahid jamal")
+    res.render("index")
 });
 
 app.listen(port, ()=>{
