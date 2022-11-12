8eee19977a8dac17e97542bcdf79f54ba2effe19
sahid jamal <sahidjamal151@gmail.com>
2022-11-12 12:44:28 +0530

    static page added

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+
+const path = require("path");
+
+const static_page = path.join(__dirname,"./public")
+//console.log(path.join(__dirname,"./public"));
+
+app.use(express.static(static_page))
 const port = 3000;
 require("./db/conn")
 app.get("/",(req,res) =>{
